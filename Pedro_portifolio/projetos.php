9d70f59b3aef4069c599cb2596b41749725ceb7c
Pedro Henrique Teixeira Pião <Pedrohtpgbi2007@gmail.com>
2025-04-05 14:43:22 -0300

    Update projetos.php

---
+++ Pedro_portifolio/projetos.php
@@ -0,0 +1,4 @@
+<!-- Marina essa página é pra mostrar os projetos que eu já fiz, eu vou deixar em baixo os links para as páginas que estão os projetos, só faz o front sendo meio que um mostruario do que eu já fiz com uma breve descrição de cada e tudo mais.
+
+//     ./projetos/
+ -->
