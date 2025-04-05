2a6d07d71dd65463b3971bc405ff6458641d3725
Pedro Henrique Teixeira Pião <Pedrohtpgbi2007@gmail.com>
2025-04-05 17:15:34 -0300

    .

--- Pedro_portifolio/projetos.php
+++ Pedro_portifolio/projetos.php
@@ -1,4 +1,13 @@
-<!-- Marina essa página é pra mostrar os projetos que eu já fiz, eu vou deixar em baixo os links para as páginas que estão os projetos, só faz o front sendo meio que um mostruario do que eu já fiz com uma breve descrição de cada e tudo mais.
+<!-- Marina essa página é pra mostrar os projetos que eu já fiz, eu vou deixar embaixo os caminhos para as páginas que estão os projetos, só faz o front sendo meio que um mostruario do que eu já fiz com uma breve descrição de cada e tudo mais.
 
-//     ./projetos/
- -->
+//     ./projetos/ ->
+            AgroTech-Brasil
+            atv03-pedro-teixeira
+            atv13 - CRUD (sequencial)*
+            atv18 - CRUD (POO)*
+            ex014
+            ex015
+            ex017
+        
+        *Precisa de um banco de dados
+-->
