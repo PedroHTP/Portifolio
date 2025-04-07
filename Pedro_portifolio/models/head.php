a102d3d5d76830a110607a3721c8528222f282bf
Pedro Henrique Teixeira Pião <Pedrohtpgbi2007@gmail.com>
2025-04-06 22:14:16 -0300

    Aplicando o front de Marina

--- Pedro_portifolio/models/head.php
+++ Pedro_portifolio/models/head.php
@@ -1,16 +1,42 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?=$title?></title>
+    
+    <!-- CSS Back-end -->
     <link rel="stylesheet" href="./styles/style.css">
-    <link rel="shortcut icon" href="./img/favicon_io/favicon.ico" type="image/x-icon">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <style>
-        body {
-            background-color: rgb(31, 30, 30);
-        }
-    </style>
+    
+    <!-- Favicon Link -->
+     <link rel="shortcut icon" href="./img/favicon_io/favicon.ico" type="image/x-icon">
+
+    <!-- bootstrap css link  -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+      crossorigin="anonymous"
+    />
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/style.css" />
+
+    <!-- bootstrap icon  -->
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css"
+    />
+
+    <!-- ADIÇÃO DE ANIMAÇÃO COM LIVRARIA AOS LINK -->
+    <link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+
+    <!-- jquery link  -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+
+
+  </head>
+
 </head>
 <script src="./scripts/script.js"></script>
--- Pedro_portifolio/models/header.php
+++ Pedro_portifolio/models/header.php
@@ -11,37 +11,60 @@
     } 
 ?>
 
-
-
-<header>
-    <div class="logo">
-        <img src="./img/favicon_io/favicon-32x32.png" alt="" id="logo">
+<nav
+  id="navbar-top"
+  class="navbar navbar-expand-lg navbar-light bg-white sticky-top"
+>
+  <div class="container-fluid px-5">
+    <a class="navbar-brand fw-bold fs-2" href="#" style="color: #3f396d"
+      >PEDRO HENRIQUE</a
+    >
+    <button
+      class="nav-menu d-md-none"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+    >
+      <i class="bi bi-menu-button-fill"></i>
+    </button>
+    <div
+      class="collapse navbar-collapse justify-content-center"
+      id="navbarNav"
+    >
+      <ul class="navbar-nav gap-4" style="font-size: 1.1rem">
+        <li class="nav-item">
+          <a class="nav-link" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#service">Serviços</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">Sobre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#portfolio">Portfolio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#testimonial">Depoimentos</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#blog">Blog</a>
+        </li>
+      </ul>
     </div>
-    <div class="header_title">
-        <h1>Portifolio</h1>
-    </div>
-    <ul>
-        <li><a href="./index.php">Home</a></li>
-        <li><a href="./biografia.php">Sobre</a></li>
-        <li><a href="./projetos.php">Projetos</a></li>
-        <li><a href="./galeria.php">Galeria</a></li>
-    </ul>
+    <button class="c-btn ms-4 py-2 px-4 rounded-pill d-none d-md-block">Contato</button>
     <div class="flex justify-end right_side">
-        <div class="pesquisa">
-            <input type="text" placeholder="Pesquisar">
-            <button><img src="./icons/search.svg" alt=""></button>
-        </div>
-
-        <div class="login" hidden>
-            <a href="#" class="link">Login</a>
-            <a href="./login.php" class="link" id="registrar">Cadastrar</a>
-        </div>
-        
-        <div class="perfil flex" id="perfil" hidden onclick="window.location.href='./perfil.php'">
-            <div><img src="./img/user.png" alt=""></div>
-            <div id="usuario">
-                <a ><?=$_SESSION['dados']['usuario'] ?? ''?></a>
-            </div>
-        </div>
-    </div>
-</header>
+  
+          <div class="login" hidden>
+              <a href="./login.php" class="link" id="registrar">Cadastrar</a>
+          </div>
+          
+          <div class="flex perfil" id="perfil" hidden onclick="window.location.href='./perfil.php'">
+              <div><img src="./img/user.png" alt=""></div>
+              <div id="usuario">
+                  <a ><?=$_SESSION['dados']['usuario'] ?? ''?></a>
+              </div>
+          </div>
+      </div>
+  </div>
+</nav>
